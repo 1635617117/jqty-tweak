49d3ee1e488168063bc27576f825dc5dfc75bcd6
1635617117 <1635617117@qq.com>
2026-08-02 23:33:24 +0800

    v10: NSURLProtocol global intercept

--- Tweak.mm
+++ Tweak.mm
@@ -3,33 +3,6 @@
 #import <UIKit/UIKit.h>
 #import <objc/runtime.h>
 
-// ========== Swizzle（改用 exchange 更可靠） ==========
-static BOOL hookMethod(Class cls, SEL sel, IMP newImp, IMP *oldImp) {
-    Method m = class_getInstanceMethod(cls, sel);
-    if (!m) {
-        NSLog(@"[JQTY] HOOK FAIL: class_getInstanceMethod nil for %@ %@", cls, NSStringFromSelector(sel));
-        return NO;
-    }
-    IMP orig = method_getImplementation(m);
-    if (orig == newImp) {
-        NSLog(@"[JQTY] HOOK SKIP: already hooked %@ %@", cls, NSStringFromSelector(sel));
-        return NO;
-    }
-    // 先加个新方法，再 exchange
-    SEL newSel = NSSelectorFromString([NSString stringWithFormat:@"__jqty_%@", NSStringFromSelector(sel)]);
-    const char *types = method_getTypeEncoding(m);
-    class_addMethod(cls, newSel, newImp, types);
-    Method newM = class_getInstanceMethod(cls, newSel);
-    method_exchangeImplementations(m, newM);
-    if (oldImp) *oldImp = method_getImplementation(newM); // exchange 后 newM 持有原始 IMP
-    NSLog(@"[JQTY] HOOK OK: %@ %@ old=%p new=%p", cls, NSStringFromSelector(sel), *oldImp, method_getImplementation(m));
-    return YES;
-}
-
-static BOOL hookClassMethod(Class cls, SEL sel, IMP newImp, IMP *oldImp) {
-    return hookMethod(object_getClass(cls), sel, newImp, oldImp);
-}
-
 // ========== 数据存储 ==========
 #define MAX_ENTRIES 300
 static NSMutableArray *entries;
@@ -51,46 +24,77 @@ static void store(NSString *type, NSString *url, NSData *data) {
         NSString *txt = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
         NSString *body = txt ? txt : hex;
         NSString *e = [NSString stringWithFormat:@"[%@] %@ %@\n%@\n---\n", ts, type, url, body];
-        @synchronized(entries) {
-            [entries addObject:e];
-            while (entries.count > MAX_ENTRIES) [entries removeObjectAtIndex:0];
-        }
+        @synchronized(entries) { [entries addObject:e]; while (entries.count > MAX_ENTRIES) [entries removeObjectAtIndex:0]; }
         dispatch_async(dispatch_get_main_queue(), ^{ if (gRefresh) gRefresh(); });
     });
 }
 
-// ========== NSURLSession Hook ==========
-static IMP orig_dataTask;
+// ========== NSURLProtocol 全局拦截 ==========
+@interface JQTYProtocol : NSURLProtocol <NSURLSessionDataDelegate>
+@property (nonatomic) NSURLSessionDataTask *task;
+@property (nonatomic) NSMutableData *respData;
+@end
 
-static id hook_dataTask(id self, SEL _cmd, NSURLRequest *req, void (^cb)(NSData *, NSURLResponse *, NSError *)) {
-    NSString *url = req.URL.absoluteString ?: @"";
-    NSLog(@"[JQTY] >>> NSURLSession dataTask: %@ %@", req.HTTPMethod, url);
-    if (req.HTTPBody) store(@"REQ", url, req.HTTPBody);
-    void (^nc)(NSData *, NSURLResponse *, NSError *) = ^(NSData *d, NSURLResponse *r, NSError *err) {
-        if (d && d.length > 0) {
-            NSInteger sc = [(NSHTTPURLResponse *)r statusCode];
-            store(@"RESP", [NSString stringWithFormat:@"%@ [%ld]", url, (long)sc], d);
-            NSLog(@"[JQTY] <<< RESP %@ [%ld] %luB", url, (long)sc, (unsigned long)d.length);
-        }
-        if (cb) cb(d, r, err);
-    };
-    return ((id (*)(id, SEL, NSURLRequest *, void (^)(NSData *, NSURLResponse *, NSError *)))orig_dataTask)(self, _cmd, req, nc);
-}
+@implementation JQTYProtocol
 
-// ========== SSL Pinning 绕过 ==========
-static IMP orig_DCAF_eval, orig_DCAF_policy, orig_WPKAF_eval, orig_UM_jb;
-static BOOL hook_DCAF_eval(id s, SEL c, SecTrustRef t, NSString *d) {
-    NSLog(@"[JQTY] DCAFSecurityPolicy evaluateServerTrust bypassed");
++ (BOOL)canInitWithRequest:(NSURLRequest *)request {
+    // 不拦截自己发出的请求（通过标记 header 判断）
+    if ([NSURLProtocol propertyForKey:@"JQTYHandled" inRequest:request]) return NO;
     return YES;
 }
-static id hook_DCAF_policy(Class cls, SEL _c, NSInteger m) {
-    id p = ((id(*)(Class, SEL, NSInteger))orig_DCAF_policy)(cls, _c, m);
-    @try { [p setValue:@YES forKey:@"allowInvalidCertificates"]; [p setValue:@NO forKey:@"validatesDomainName"]; [p setValue:@(0) forKey:@"SSLPinningMode"]; } @catch(...){}
-    NSLog(@"[JQTY] DCAFSecurityPolicy policyWithPinningMode bypassed mode=%ld", (long)m);
-    return p;
+
++ (NSURLRequest *)canonicalRequestForRequest:(NSURLRequest *)request {
+    return request;
 }
-static BOOL hook_WPKAF_eval(id s, SEL c, SecTrustRef t, NSString *d) { return YES; }
-static BOOL hook_UM_jb(Class c, SEL s) { NSLog(@"[JQTY] UMConfigure isJailbreak -> NO"); return NO; }
+
+- (void)startLoading {
+    NSMutableURLRequest *req = [self.request mutableCopy];
+    [NSURLProtocol setProperty:@YES forKey:@"JQTYHandled" inRequest:req];
+    
+    // 记录请求
+    NSString *url = req.URL.absoluteString ?: @"";
+    NSString *method = req.HTTPMethod ?: @"GET";
+    if (req.HTTPBody) store(@"REQ", [NSString stringWithFormat:@"%@ %@", method, url], req.HTTPBody);
+    
+    NSURLSessionConfiguration *cfg = [NSURLSessionConfiguration defaultSessionConfiguration];
+    cfg.protocolClasses = @[];
+    NSURLSession *s = [NSURLSession sessionWithConfiguration:cfg delegate:self delegateQueue:nil];
+    self.task = [s dataTaskWithRequest:req];
+    self.respData = [NSMutableData data];
+    [self.task resume];
+}
+
+- (void)stopLoading {
+    [self.task cancel];
+    self.task = nil;
+}
+
+- (void)URLSession:(NSURLSession *)session dataTask:(NSURLSessionDataTask *)dataTask didReceiveData:(NSData *)data {
+    [self.respData appendData:data];
+    [self.client URLProtocol:self didLoadData:data];
+}
+
+- (void)URLSession:(NSURLSession *)session dataTask:(NSURLSessionDataTask *)dataTask
+ didReceiveResponse:(NSURLResponse *)response completionHandler:(void (^)(NSURLSessionResponseDisposition))completionHandler {
+    [self.client URLProtocol:self didReceiveResponse:response cacheStoragePolicy:NSURLCacheStorageNotAllowed];
+    completionHandler(NSURLSessionResponseAllow);
+}
+
+- (void)URLSession:(NSURLSession *)session task:(NSURLSessionTask *)task didCompleteWithError:(NSError *)error {
+    if (error) {
+        [self.client URLProtocol:self didFailWithError:error];
+    } else {
+        // 记录响应
+        NSString *url = self.request.URL.absoluteString ?: @"";
+        NSString *method = self.request.HTTPMethod ?: @"GET";
+        NSHTTPURLResponse *httpResp = (NSHTTPURLResponse *)task.response;
+        store(@"RESP", [NSString stringWithFormat:@"%@ %@ [%ld]", method, url, (long)httpResp.statusCode], self.respData);
+        [self.client URLProtocolDidFinishLoading:self];
+    }
+    [session finishTasksAndInvalidate];
+}
+
+@end
 
 // ========== 悬浮窗 ==========
 @interface JQTYWin : UIWindow
@@ -100,6 +104,7 @@ static BOOL hook_UM_jb(Class c, SEL s) { NSLog(@"[JQTY] UMConfigure isJailbreak 
 @implementation JQTYWin {
     UIButton *_tBtn, *_lBtn, *_cBtn;
     UITextView *_tv;
+    UILabel *_st;
     BOOL _exp;
 }
 
@@ -118,9 +123,9 @@ static BOOL hook_UM_jb(Class c, SEL s) { NSLog(@"[JQTY] UMConfigure isJailbreak 
         [self addGestureRecognizer:p];
 
         CGFloat bw = (self.bounds.size.width - 20) / 3;
-        _tBtn = [self makeBtn:@"● OFF" act:@selector(tTap) frame:CGRectMake(5, 2, bw, 40)];
-        _lBtn = [self makeBtn:@"LOG" act:@selector(lTap) frame:CGRectMake(10+bw, 2, bw, 40)];
-        _cBtn = [self makeBtn:@"CLR" act:@selector(cTap) frame:CGRectMake(15+bw*2, 2, bw, 40)];
+        _tBtn = [self btn:@"● OFF" act:@selector(tTap) f:CGRectMake(5, 2, bw, 40)];
+        _lBtn = [self btn:@"LOG" act:@selector(lTap) f:CGRectMake(10+bw, 2, bw, 40)];
+        _cBtn = [self btn:@"CLR" act:@selector(cTap) f:CGRectMake(15+bw*2, 2, bw, 40)];
         [self addSubview:_tBtn]; [self addSubview:_lBtn]; [self addSubview:_cBtn];
 
         _tv = [[UITextView alloc] init];
@@ -129,11 +134,17 @@ static BOOL hook_UM_jb(Class c, SEL s) { NSLog(@"[JQTY] UMConfigure isJailbreak 
         _tv.font = [UIFont fontWithName:@"Courier" size:10] ?: [UIFont systemFontOfSize:10];
         _tv.editable = NO; _tv.hidden = YES;
         [self addSubview:_tv];
+        
+        _st = [[UILabel alloc] initWithFrame:CGRectMake(8, self.bounds.size.height - 14, self.bounds.size.width - 16, 12)];
+        _st.font = [UIFont systemFontOfSize:9];
+        _st.textColor = [UIColor colorWithRed:0 green:1 blue:0.5 alpha:0.7];
+        _st.text = @"ready";
+        [self addSubview:_st];
     }
     return self;
 }
 
-- (UIButton *)makeBtn:(NSString *)t act:(SEL)a frame:(CGRect)f {
+- (UIButton *)btn:(NSString *)t act:(SEL)a f:(CGRect)f {
     UIButton *b = [UIButton buttonWithType:UIButtonTypeSystem];
     b.frame = f; b.titleLabel.font = [UIFont boldSystemFontOfSize:12];
     [b setTitle:t forState:UIControlStateNormal]; b.tintColor = [UIColor whiteColor];
@@ -141,18 +152,20 @@ static BOOL hook_UM_jb(Class c, SEL s) { NSLog(@"[JQTY] UMConfigure isJailbreak 
     return b;
 }
 
+- (void)setStatus:(NSString *)s { _st.text = s; }
+
 - (void)tTap {
     gOn = !gOn;
     if (gOn) {
         [_tBtn setTitle:@"● REC" forState:UIControlStateNormal];
         _tBtn.tintColor = [UIColor colorWithRed:0 green:1 blue:0.5 alpha:1];
         self.layer.borderColor = [UIColor colorWithRed:0 green:1 blue:0.5 alpha:1].CGColor;
-        NSLog(@"[JQTY] CAPTURE ON");
+        _st.text = @"capturing...";
     } else {
         [_tBtn setTitle:@"○ OFF" forState:UIControlStateNormal];
         _tBtn.tintColor = [UIColor whiteColor];
         self.layer.borderColor = [UIColor colorWithRed:0 green:1 blue:0.5 alpha:0.6].CGColor;
-        NSLog(@"[JQTY] CAPTURE OFF");
+        _st.text = @"stopped";
     }
 }
 
@@ -163,18 +176,24 @@ static BOOL hook_UM_jb(Class c, SEL s) { NSLog(@"[JQTY] UMConfigure isJailbreak 
     if (_exp) {
         CGFloat w = s.size.width - 16;
         self.frame = CGRectMake(8, 60, w, s.size.height - 120);
-        _tv.hidden = NO; _tv.frame = CGRectMake(4, 50, w - 8, self.bounds.size.height - 58);
+        _tv.hidden = NO;
+        _tv.frame = CGRectMake(4, 50, w - 8, self.bounds.size.height - 68);
+        _st.frame = CGRectMake(8, self.bounds.size.height - 14, w - 16, 12);
         [self refresh];
     } else {
         self.frame = CGRectMake(30, s.size.height - 164, s.size.width - 60, 44);
         _tv.hidden = YES;
+        _st.frame = CGRectMake(8, 30, self.bounds.size.width - 16, 12);
     }
+    // relayout buttons
+    _tBtn.frame = CGRectMake(5, 2, bw, 40);
+    _lBtn.frame = CGRectMake(10+bw, 2, bw, 40);
+    _cBtn.frame = CGRectMake(15+bw*2, 2, bw, 40);
 }
 
 - (void)cTap {
     @synchronized(entries) { [entries removeAllObjects]; }
     _tv.text = @"";
-    NSLog(@"[JQTY] LOG CLEARED");
 }
 
 - (void)refresh {
@@ -182,6 +201,8 @@ static BOOL hook_UM_jb(Class c, SEL s) { NSLog(@"[JQTY] UMConfigure isJailbreak 
         _tv.text = [entries componentsJoinedByString:@""];
     }
     if (_tv.text.length > 0) [_tv scrollRangeToVisible:NSMakeRange(_tv.text.length - 1, 1)];
+    NSUInteger n = entries.count;
+    _st.text = n > 0 ? [NSString stringWithFormat:@"%lu entries", (unsigned long)n] : @"ready";
 }
 
 - (void)pan:(UIPanGestureRecognizer *)p {
@@ -192,39 +213,52 @@ static BOOL hook_UM_jb(Class c, SEL s) { NSLog(@"[JQTY] UMConfigure isJailbreak 
 }
 @end
 
+// ========== SSL Pinning 绕过 (runtime swizzle) ==========
+static BOOL hookMethod(Class cls, SEL sel, IMP newImp, IMP *oldImp) {
+    Method m = class_getInstanceMethod(cls, sel);
+    if (!m) return NO;
+    SEL newSel = NSSelectorFromString([NSString stringWithFormat:@"__jqty_%@", NSStringFromSelector(sel)]);
+    class_addMethod(cls, newSel, newImp, method_getTypeEncoding(m));
+    Method newM = class_getInstanceMethod(cls, newSel);
+    method_exchangeImplementations(m, newM);
+    if (oldImp) *oldImp = method_getImplementation(newM);
+    return YES;
+}
+static BOOL hookClassMethod(Class cls, SEL sel, IMP newImp, IMP *oldImp) {
+    return hookMethod(object_getClass(cls), sel, newImp, oldImp);
+}
+
+static IMP od_eval, od_pol, ow_eval, oum_jb;
+static BOOL hk_eval(id s, SEL c, SecTrustRef t, NSString *d) { return YES; }
+static id hk_pol(Class c, SEL s, NSInteger m) {
+    id p = ((id(*)(Class, SEL, NSInteger))od_pol)(c, s, m);
+    @try { [p setValue:@YES forKey:@"allowInvalidCertificates"]; [p setValue:@NO forKey:@"validatesDomainName"]; [p setValue:@(0) forKey:@"SSLPinningMode"]; } @catch(...){}
+    return p;
+}
+static BOOL hk_weval(id s, SEL c, SecTrustRef t, NSString *d) { return YES; }
+static BOOL hk_umjb(Class c, SEL s) { return NO; }
+
 // ========== 入口 ==========
 static JQTYWin *gWin;
 
 __attribute__((constructor))
 static void init(void) {
-    NSLog(@"[JQTY] ===== dylib loaded =====");
     entries = [NSMutableArray array];
-
+    
+    // 注册 NSURLProtocol（尽早）
+    [NSURLProtocol registerClass:[JQTYProtocol class]];
+    
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 1 * NSEC_PER_SEC), dispatch_get_main_queue(), ^{
-        NSLog(@"[JQTY] --- installing hooks ---");
-        
-        BOOL ok1 = hookMethod(NSClassFromString(@"NSURLSession"),
-                              @selector(dataTaskWithRequest:completionHandler:),
-                              (IMP)hook_dataTask, &orig_dataTask);
-        NSLog(@"[JQTY] NSURLSession hook: %@", ok1 ? @"YES" : @"NO");
-        
         Class dc = NSClassFromString(@"DCAFSecurityPolicy");
-        if (dc) {
-            hookMethod(dc, @selector(evaluateServerTrust:forDomain:), (IMP)hook_DCAF_eval, &orig_DCAF_eval);
-            hookClassMethod(dc, @selector(policyWithPinningMode:), (IMP)hook_DCAF_policy, &orig_DCAF_policy);
-        }
+        if (dc) { hookMethod(dc, @selector(evaluateServerTrust:forDomain:), (IMP)hk_eval, &od_eval); hookClassMethod(dc, @selector(policyWithPinningMode:), (IMP)hk_pol, &od_pol); }
         Class wk = NSClassFromString(@"WPKAFSecurityPolicy");
-        if (wk) hookMethod(wk, @selector(evaluateServerTrust:forDomain:), (IMP)hook_WPKAF_eval, &orig_WPKAF_eval);
+        if (wk) hookMethod(wk, @selector(evaluateServerTrust:forDomain:), (IMP)hk_weval, &ow_eval);
         Class um = NSClassFromString(@"UMConfigure");
-        if (um) hookClassMethod(um, @selector(isJailbreak), (IMP)hook_UM_jb, &orig_UM_jb);
-        
-        NSLog(@"[JQTY] --- hooks done ---");
+        if (um) hookClassMethod(um, @selector(isJailbreak), (IMP)hk_umjb, &oum_jb);
     });
-
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 2 * NSEC_PER_SEC), dispatch_get_main_queue(), ^{
-        NSLog(@"[JQTY] --- creating window ---");
+    
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 1.5 * NSEC_PER_SEC), dispatch_get_main_queue(), ^{
         gWin = [[JQTYWin alloc] init];
         gRefresh = ^{ [gWin refresh]; };
-        NSLog(@"[JQTY] --- window created ---");
     });
 }
